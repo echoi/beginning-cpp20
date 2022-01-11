f36dc7afd72ae550562397695a80addaf6f80ed2
Ryann Lam <rclam@Ryanns-MBP.attlocal.net>
2022-01-11 12:05:59 -0600

    changes from class exercise

--- Examples/Modules/Chapter 06/Ex6_02.cpp
+++ Examples/Modules/Chapter 06/Ex6_02.cpp
@@ -1,7 +1,7 @@
 // Dereferencing pointers
 // Calculates the purchase price for a given quantity of items
-import <iostream>;
-import <format>;
+#include <iostream>
+//import <format>;
 
 int main()
 {
@@ -9,16 +9,19 @@ int main()
   int count {};                        // Number of items ordered
   int discount_threshold {25};         // Quantity threshold for discount
   double discount {0.07};              // Discount for quantities over discount_threshold
-
-  int* pcount {&count};                // Pointer to count
+  
+  int *pcount {&count};                // Pointer to count
+  std::cout << "count is currently " << count << std::endl;
   std::cout << "Enter the number of items you want: ";
   std::cin >> *pcount;
-  std::cout << std::format("The unit price is ${:.2f}\n", unit_price / 100.0);
+  std::cout << "count is now " << count << std::endl;
+  std::cout << "pcount is " << pcount << std::endl;
+  std::cout << "The unit price is: " << (unit_price / 100.0) << std::endl;
 
   // Calculate gross price
   int* punit_price{ &unit_price };      // Pointer to unit_price
-  int price{ *pcount * *punit_price };  // Gross price via pointers
-  auto* pprice {&price};                // Pointer to gross price
+  int price{ (*pcount) * (*punit_price) };  // Gross price via pointers
+  auto *pprice {&price};                // Pointer to gross price
 
   // Calculate net price in US$
   double net_price{};
@@ -26,13 +29,12 @@ int main()
   pnet_price = &net_price;
   if (*pcount > discount_threshold)
   {
-    std::cout << 
-      std::format("You qualify for a discount of {:.0f} percent.\n", discount * 100);
+    std::cout << "You qualify for a discount of " << (discount*100) << " percent." << std::endl;
     *pnet_price = price*(1 - discount) / 100;
   }
   else
   {
     net_price = *pprice / 100;
   }
-  std::cout << std::format("The net price for {} items is ${:.2f}\n", *pcount, net_price);
+  std::cout << "The net price for " << *pcount <<  " items is " << net_price << std::endl;
 }
