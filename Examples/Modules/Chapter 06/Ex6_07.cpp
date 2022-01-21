215d2d5ecc06d97604b7f7046f9b58ed7959558a
Ryann Lam <rclam@Ryanns-MBP.attlocal.net>
2022-01-21 13:47:58 -0600

    class working example

--- Examples/Modules/Chapter 06/Ex6_07.cpp
+++ Examples/Modules/Chapter 06/Ex6_07.cpp
@@ -1,14 +1,22 @@
 // Using smart pointers
-import <iostream>;
-import <format>;
-import <memory>;   // For smart pointers
-import <vector>;   // For std::vector<> container
+#include <iostream>
+//import <format>;
+#include <memory>   // For smart pointers
+#include <vector>   // For std::vector<> container
 
 #include <cctype>  // For std::toupper()
 
+#include <iomanip>  // for setprecision
+
+
 int main()
 {
+  
   std::vector<std::shared_ptr<std::vector<double>>> records; // Temperature records by days
+  // records[0] => shared_ptr to [0.0, 0.1, 11.0, 13.0]    // very cold day
+  // records[1] => shared_ptr to [10.0, 11.1, 13.0, 15.0, 17.0, 19.0]  // warm day
+  // records[2] => shared_ptr to [1.0, 2.1] // cold again
+
   size_t day{ 1 };       // Day number
 
   while (true)           // Collect temperatures by day
@@ -29,7 +37,7 @@ int main()
       day_records->push_back(t);
     }
 
-    std::cout << "Enter another day's temperatures (Y or N)? ";
+    std::cout << "Enter another day's temperatures (Y or N)? "; 
     char answer{};
     std::cin >> answer;
     if (std::toupper(answer) != 'Y') break;
@@ -42,14 +50,17 @@ int main()
     double total{};
     size_t count{};
 
-    std::cout << std::format("\nTemperatures for day {}:\n", day++);
+    //std::cout << std::format("\nTemperatures for day {}:\n", day++);
+    std::cout << "\nTemperatures for day " << day++ << ":\n";
     for (auto temp : *record)
     {
       total += temp;
-      std::cout << std::format("{:6.2f}", temp);
+      //std::cout << std::format("{:6.2f}", temp);
+      std::cout << std::setprecision(2) << std::setw(5) << temp;
       if (++count % 5 == 0) std::cout << std::endl;
     }
 
-    std::cout << std::format("\nAverage temperature: {:.2f}", total / count) << std::endl;
+    //std::cout << std::format("\nAverage temperature: {:.2f}", total / count) << std::endl;
+    std::cout << "\nAverage temperature: " << std::setprecision(2) << total/count << std::endl;
   }
 }
