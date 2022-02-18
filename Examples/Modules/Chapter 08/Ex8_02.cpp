a9454b1ebcac0856c031e502ac4d0a57911cecab
R.C. Lam <rclam@memphis.edu>
2022-02-18 14:02:40 -0600

    class exercise edits

--- Examples/Modules/Chapter 08/Ex8_02.cpp
+++ Examples/Modules/Chapter 08/Ex8_02.cpp
@@ -1,17 +1,33 @@
 // Calculating powers - rearranged
-import <iostream>;
-import <format>;
+// import <iostream>;
+// import <format>;
+#include <iostream>
+#include <iomanip>
+// #include <format>
 
-//double power(double x, int n);               // Function prototype - uncomment for successful compilation
+// Declare and define a function that takes two numbers and multiply them and return the product.
+double product( int number1, double number2 );
+double product( double number1, double number2);
+double power(double x, int n);               // Function prototype - uncomment for successful compilation
 
 int main()
 {
   // Calculate powers of 8 from -3 to +3
   for (int i{ -3 }; i <= 3; ++i)
-    std::cout << std::format("{:10}", power(8.0, i));
+    // std::cout << std::format("{:10}", power(8.0, i));
+    std::cout << std::setw(10) << power(8.0, i);
 
   std::cout << std::endl;
+
+  std::cout << product( 4.167, -100 ) << std::endl;
 }
+
+// int 4 bytes    = 32 bits   1 --> 1.00000000000000000
+// float 4 bytes  = 32 bits   1.000000 --> 1.0000000000000000
+// double 8 bytes = 64 bits
+// double num = 3.141592129739817927397123; 
+// double -> float: 3.14159212973
+// double -> int: 3 
 
 // Function to calculate x to the power n
 double power(double x, int n)
@@ -30,3 +46,21 @@ double power(double x, int n)
   return result;
 }
 
+// Function to calculate product of two float. pt num
+double product( int a, double b)
+//double product( double a, double b)
+{
+  std::cout << "(1)" << std::endl;
+  double result{};
+  result = a * b;
+  return result;
+}
+
+// Function to calculate product of two float. pt num
+double product( double a, double b)
+{
+  std::cout << "(2)" << std::endl;
+  double result{};
+  result = a * b;
+  return result;
+}
