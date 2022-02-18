5ba6148461254b0657c90c3703acf889242d20ec
R.C. Lam <rclam@memphis.edu>
2022-02-18 14:02:14 -0600

    class exercise edits

--- Examples/Modules/Chapter 08/Ex8_01.cpp
+++ Examples/Modules/Chapter 08/Ex8_01.cpp
@@ -1,7 +1,9 @@
 // Calculating powers
-import <iostream>;
-import <format>;
+#include <iostream>
+#include <iomanip>
+//import <format>;
 
+// function definition.
 // Function to calculate x to the power n
 double power(double x, int n)
 {
@@ -23,8 +25,11 @@ int main()
 {
   // Calculate powers of 8 from -3 to +3
   for (int i{ -3 }; i <= 3; ++i)
-    std::cout << std::format("{:10g}", power(8.0, i));
-
+    // std::cout << std::format("{:10g}", power(8.0, i));
+    std::cout << std::setw(10) << power(8.0, i);
+    // f: floating point number  xx.yyyy 123.123456
+    // g: exponential form or floating: 1.234e+10, 0.12234
+    // e: exponential 
   std::cout << std::endl;
 }
 
